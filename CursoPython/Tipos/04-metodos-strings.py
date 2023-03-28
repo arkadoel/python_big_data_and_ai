b4f1648018fea487fae4d563ae04dde511f8e5aa
arkadoel <fer.d.minguela@gmail.com>
2023-03-28 21:57:21 +0200

    seguir

---
+++ CursoPython/Tipos/04-metodos-strings.py
@@ -0,0 +1,13 @@
+animal = " perrito FELIZ "
+print(animal.upper())
+print(animal.lower())
+print(animal.strip().capitalize())
+print(animal.title())
+print(animal.strip())  # quitar espacios delante y detras
+print(animal.lstrip())  # quitar espacios delante y detras
+print(animal.rstrip())  # quitar espacios delante y detras
+print(animal.find("rr"))
+print(animal.find("aaaa"))
+print(animal.replace("rr", "gu"))
+print("rr" in animal)
+print("rr" not in animal)
