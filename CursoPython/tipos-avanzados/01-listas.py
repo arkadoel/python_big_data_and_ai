b659139a26e1c07193284dc5b18c224188d47552
arkadoel <fer.d.minguela@gmail.com>
2023-04-25 20:50:59 +0200

    listas

---
+++ CursoPython/tipos-avanzados/01-listas.py
@@ -0,0 +1,23 @@
+numeros = [1, 2, 3]
+print(numeros)
+
+letras = ["a", "b", "c"]
+print(letras)
+
+palabras = ["gatito", "feliz"]
+palabras_felices = ["gatito", "feliz", "Felipe", "alumno"]
+booleans = [True, False, True, True]
+matriz = [[0, 1], [1, 0]]
+print(matriz)
+
+ceros = [0] * 10  # va a hacer un array de diez ceros
+print(ceros)
+ceros = [0, 1] * 10
+print(ceros)
+
+alfanumerico = numeros + letras
+print(alfanumerico)
+crear_nueva_list = list()
+rango = list(range(1, 11))  # listado del 1 al 10
+caracteres = list("hola mundo")
+print(caracteres)
